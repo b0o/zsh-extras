fd8f9cc186d57cf8290fe2a8a53f4cf8d41267be
Maddison Hellstrom <maddy@na.ai>
2021-06-12 19:59:23 -0700

    Build zsh-extras.plugin.zsh

--- zsh-extras.plugin.zsh
+++ zsh-extras.plugin.zsh
@@ -3,18 +3,10 @@
 # Repo:    https://github.com/b0o/zsh-extras
 # License: MIT
 
+# NOTICE: THIS FILE IS AUTO-GENERATED WITH MAKE
+
 fpath+=("${0:h}/functions")
 
-# Generated with:
-#
-# () {
-#   echo -n 'autoload -Uz'
-#   for f in functions/[^_]*
-#     do printf ' \\\n  "%s"' "$(basename "$f")"
-#   done
-#   echo
-# }
-#
 autoload -Uz \
   "mg" \
   "nvim-help" \
